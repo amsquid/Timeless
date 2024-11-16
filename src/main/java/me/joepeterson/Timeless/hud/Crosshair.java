b8db16c5096e596400b21d7b4a6fca4052bbae7e
Joseph Peterson <supercrazysquid@gmail.com>
2024-11-16 09:02:07 -0500

    Changed crosshair sizing

--- src/main/java/me/joepeterson/Timeless/hud/Crosshair.java
+++ src/main/java/me/joepeterson/Timeless/hud/Crosshair.java
@@ -12,7 +12,7 @@ public class Crosshair extends HUDItem {
 	public Crosshair() throws IOException {
 		Texture texture = new Texture("textures/ui/crosshair.png");
 
-		Vector2f scale = Vector.multiplyVector(new Vector2f(1 / 16.f, 1 / 9.f), .6f);
+		Vector2f scale = Vector.multiplyVector(new Vector2f(1 / 16.f, 1 / 9.f), .4f);
 		Vector2f position = new Vector2f(0.5f - (scale.x / 2.0f), 0.5f - (scale.y / 2.0f));
 
 		super(texture, position, scale);
