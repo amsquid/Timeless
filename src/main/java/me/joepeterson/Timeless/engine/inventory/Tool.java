18ef35a07afd402c2c91b2f840f62e187be69908
Joseph Peterson <supercrazysquid@gmail.com>
2025-01-16 08:59:22 -0500

    Tabs

--- src/main/java/me/joepeterson/Timeless/engine/inventory/Tool.java
+++ src/main/java/me/joepeterson/Timeless/engine/inventory/Tool.java
@@ -3,23 +3,23 @@ package me.joepeterson.Timeless.engine.inventory;
 import me.joepeterson.Timeless.engine.texture.Texture;
 
 public class Tool extends Item {
-    private double durability;
-    private final double durabilityMax;
+	private double durability;
+	private final double durabilityMax;
 
-    public Tool(int itemStackSize, String name, Texture texture, double durabilityMax) {
-        super(itemStackSize, name, texture);
-        this.durabilityMax = durabilityMax;
-    }
+	public Tool(int itemStackSize, String name, Texture texture, double durabilityMax) {
+		super(itemStackSize, name, texture);
+		this.durabilityMax = durabilityMax;
+	}
 
-    public double getDurability() {
-        return durability;
-    }
+	public double getDurability() {
+		return durability;
+	}
 
-    public double getDurabilityMax() {
-        return durabilityMax;
-    }
+	public double getDurabilityMax() {
+		return durabilityMax;
+	}
 
-    public void setDurability(double durability) {
-        this.durability = durability;
-    }
+	public void setDurability(double durability) {
+		this.durability = durability;
+	}
 }
